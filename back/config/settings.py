9ace8b219d4bc030eddf9b60a864df11794ecaeb
Andrey Perevyazko <110488818+Perevyazko1@users.noreply.github.com>
2026-02-22 10:52:42 +0300

    add django-unfold

--- back/config/settings.py
+++ back/config/settings.py
@@ -13,7 +13,6 @@ DEBUG = int(os.getenv('DEBUG', 0))
 
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost 127.0.0.1').split()
 
-
 INSTALLED_APPS = [
     'unfold',
     'unfold.contrib.filters',
